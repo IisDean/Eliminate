f39e23220edcb38515ca681590cb578a70f515d5
Liu min <781775946@qq.com>
2017-11-21 18:12:52 +0800

    11月21日位置互换增加动画

--- src/Main.ts
+++ src/Main.ts
@@ -127,17 +127,22 @@ class Main extends egret.DisplayObjectContainer{
 
     //滑动
     private isTouch(evt:egret.TouchEvent){
-         if(!this.touch)return false;//滑动阻止
-         this.touch = false;//滑动状态变更
+        //滑动阻止
+         if(!this.touch)return false;
+         //滑动状态变更
+         this.touch = false;
          var touchStart = this.gameObj['coords']['start'];
          var touchOption = this.option['gameCoords'];
-         if( touchStart[0] < touchOption['minX'] || touchStart[0] > touchOption['maxX'] || touchStart[1] < touchOption['minH'] || touchStart[1] > touchOption['maxH'] )return false;//不在游戏区域
-         this.gameObj['iconObj'] = this.seachIconObj(this.gameObj['coords']['start'][0],this.gameObj['coords']['start'][1]);//寻找滑动的图标对象
+         //不在游戏区域
+         if( touchStart[0] < touchOption['minX'] || touchStart[0] > touchOption['maxX'] || touchStart[1] < touchOption['minH'] || touchStart[1] > touchOption['maxH'] )return false;
+         //寻找滑动的图标对象
+         this.gameObj['iconObj'] = this.seachIconObj(this.gameObj['coords']['start'][0],this.gameObj['coords']['start'][1]);
          this.gameObj['coords']['move'][0] = evt.localX;
          this.gameObj['coords']['move'][1] = evt.localY;
          var x = this.gameObj['coords']['move'][0] - touchStart[0],
             y = this.gameObj['coords']['move'][1] - touchStart[1];
-        this.gameObj['direction'] = this.isDirection(x,y);//滑动方向
+        //滑动方向
+        this.gameObj['direction'] = this.isDirection(x,y);
         var aX = this.gameObj['iconObj'][0],
             aY = this.gameObj['iconObj'][1];
         this.locaChange(this.gameObj['direction'],aX,aY);
@@ -162,24 +167,31 @@ class Main extends egret.DisplayObjectContainer{
                 a -= 1;
             break;
         }
+        // 判断边界
         if( c + a >= row[0] || c + a < 0 || d + b >= row[1] || d + b < 0 ){
             return false;
         }
+        //位置互换
         var e = this.option['gameArr'][c][d]['obj'];
         this.option['gameArr'][c][d]['obj'] = this.option['gameArr'][c+a][d+b]['obj']
         this.option['gameArr'][c+a][d+b]['obj'] = e;
-        
+        //位置移动
         var w = this.option['kid']['width'],
             h = this.option['kid']['height'];
-        this.option['gameArr'][c][d]['obj']['dom'].x = this.option['gameArr'][c][d]['loca'][0]-w;
-        this.option['gameArr'][c][d]['obj']['dom'].y = this.option['gameArr'][c][d]['loca'][1]-h;
-        this.option['gameArr'][c+a][d+b]['obj']['dom'].x = this.option['gameArr'][c+a][d+b]['loca'][0]-w;
-        this.option['gameArr'][c+a][d+b]['obj']['dom'].y = this.option['gameArr'][c+a][d+b]['loca'][1]-h;
+        var activeDom = this.option['gameArr'][c][d],
+            nextDom = this.option['gameArr'][c+a][d+b];
+        this.moveAnimation(activeDom['obj']['dom'],[activeDom['loca'][0]-w,activeDom['loca'][1]-h],150);
+        this.moveAnimation(nextDom['obj']['dom'],[nextDom['loca'][0]-w,nextDom['loca'][1]-h],150);
         this.activeEli = {
             activeArr : [],
             index : null,
         };
         this.detection(c,d);
+    }
+
+    //缓动效果
+    private moveAnimation(obj,loca,time){
+        egret.Tween.get( obj ).to( {x:loca[0],y:loca[1] }, time, egret.Ease.sineIn );
     }
 
     //滑动方向判断
@@ -218,19 +230,27 @@ class Main extends egret.DisplayObjectContainer{
         index : null,
     };
 
+    //寻找消除对象
     private detection(x,y){
         var a = this.option['gameArr'];
         if(this.activeEli['index'] == null ){
-            this.activeEli['activeArr'].push({x:x,y:y,status:1});
+            this.activeEli['activeArr'].push({x:x,y:y});
             this.activeEli['index'] = a[x][y]['obj']['index'];
         }
-        // 左-1
-        if( x > 0 && a[x-1][y]['obj']['index'] == this.activeEli['index'] ){
-            this.activeEli['activeArr'].push({x:x-1,y:y,status:1,});
+        
+        if( x > 0 && a[x-1][y]['obj']['index'] == this.activeEli['index'] ){// 左-1
+            this.activeEli['activeArr'].push({x:x-1,y:y});
             this.detection(x-1,y);
+        }else if( x < this.option['row'][0] && a[x+1][y]['obj']['index'] == this.activeEli['index'] ){//右
+            this.activeEli['activeArr'].push({x:x+1,y:y});
+            this.detection(x+1,y);
+        }else if( y > 0 && a[x][y-1]['obj']['index'] == this.activeEli['index'] ){//上
+            this.activeEli['activeArr'].push({x:x,y:y-1});
+            this.detection(x,y-1);
+        }else if( y < this.option['row'][1] && a[x][y+1]['obj']['index'] == this.activeEli['index'] ){//下
+            this.activeEli['activeArr'].push({x:x,y:y+1});
+            this.detection(x,y+1);
         }
-        console.log(x+'--'+y);
-        console.log(x-1+'--'+y);
         console.log(this.activeEli);
     }
 
